5f9ea9636c3a9b7ec81b7cbb80cdc839afe68218
Preetam Singh <spreetam2001@gmail.com>
2023-09-18 10:22:50 +0530

    intital

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css"
+import  ReactLoading from "react-loading";
 
-function App() {
+const App = () => {
+  const [Images, setImages] = useState([]);
+  const [pages, setPages] = useState(1);
+  const [loading,setLoading]=useState(true);
+
+  const getImages = async (pageno) => {
+    const url = `https://api.unsplash.com/photos/random?client_id=Qe2QR0oQ4bmVEHKWdBjhY3RcCQus3ear2jvSUnnoe1U&count=9&page=${pageno}`;
+    await Axios.get(url).then((res) => {
+      setImages((prev) => [...prev, ...res.data]);
+      setLoading(false);
+    },400);
+  };
+
+  const handleScroll = async () => {
+    try {
+      if (
+        window.innerHeight + document.documentElement.scrollTop + 1 >=
+        document.documentElement.scrollHeight
+      ) {
+        setLoading(true);
+        setPages((prev) => prev + 1);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getImages(pages);
+  }, [pages]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h3 className="heading">Lazy Loading Website</h3>
+        <div className="Container">
+          {Images.map((image) => {
+            return <img src={image.urls.regular} key={image.id} />;
+          })}
+        </div>
+          <div className="Product-Loading">
+          {loading && <ReactLoading
+            type="spinningBubbles"
+            color="#764abc"
+            height={300}
+            width={120}
+          />}
+        </div>
+        
+      </div>
+    </>
   );
-}
+};
 
 export default App;
